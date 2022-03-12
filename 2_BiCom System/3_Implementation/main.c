4a925a42eca12f5edcb33ea2f00434ea5140e144
Kowsik15 <98818618+Kowsik15@users.noreply.github.com>
2022-03-12 09:26:42 +0530

    Update main.c

--- 2_BiCom System/3_Implementation/main.c
+++ 2_BiCom System/3_Implementation/main.c
@@ -5,12 +5,12 @@
 #include "driver.h"
 #include <stdio.h>
 
-static void wind_stat(void); // here this function will Turn on all the LED's at once
-static void alrm_stat(void); // here this function will Turn off all the LED's at once
-static void door_stat(void); // here this function will on and off LED's with a certain delay (in anti-clockwise)
-static void bat_info(void);	 // here this function will on and off LED's with a certain delay (in clockwise)
+static void wind_stat(void); // Turn on all the LED's at once
+static void alrm_stat(void); // Turn off all the LED's at once
+static void door_stat(void); // on and off LED's with a certain delay (in anti-clockwise)
+static void bat_info(void); // on and off LED's with a certain delay (in clockwise)
 
-static void wind_stat(void) // This function is done by Mantra G with help of Nyalam Praveenraj
+static void wind_stat(void) 
 {
 	led_on(LED_ORANGE);
 	led_on(LED_GREEN);
@@ -18,14 +18,14 @@ static void wind_stat(void) // This function is done by Mantra G with help of Ny
 	led_on(LED_BLUE);
 }
 
-static void alrm_stat(void) // This function is done by Mantra G with help of Nyalam Praveenraj
+static void alrm_stat(void) 
 {
 	led_off(LED_ORANGE);
 	led_off(LED_GREEN);
 	led_off(LED_RED);
 	led_off(LED_BLUE);
 }
-static void bat_info(void) // This function is done by bashaboina Naresh with help of Nyalam Praveenraj
+static void bat_info(void) // 
 {
 
 	led_on(LED_GREEN);
@@ -45,7 +45,7 @@ static void bat_info(void) // This function is done by bashaboina Naresh with he
 	led_off(LED_GREEN);
 }
 
-static void door_stat(void) // This function is done by Varun S with help of Nyalam Praveenraj
+static void door_stat(void) 
 {
 
 	led_on(LED_GREEN);
@@ -66,7 +66,7 @@ static void door_stat(void) // This function is done by Varun S with help of Nya
 }
 
 int main(void)
-{ // main function is done by bashaboina Naresh, Varun S, Mantra G with help of Nyalam Praveenraj
+{ 
 
 	evr_led();
 	btn_on();
@@ -75,23 +75,23 @@ int main(void)
 	{
 		volatile int btncnt = btn_chk();
 		if (btncnt == 1)
-		{ // comparing the no of cnts of button
+		{ 
 			wind_stat();
 			btncnt = 0;
 		}
 		else if (btncnt == 2)
-		{ // comparing the no of cnts of button
+		{ 
 			alrm_stat();
 			btncnt = 0;
 		}
 		else if (btncnt == 3)
-		{ // comparing the no of cnts of button
+		{ 
 			bat_info();
 
 			btncnt = 0;
 		}
 		else if (btncnt == 4)
-		{ // comparing the no of cnts of button
+		{ 
 			door_stat();
 			btncnt = 0;
 		}
